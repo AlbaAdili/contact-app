1c56ee67bce2d5568d5ee06ef66d5c75e658f967
Alba <aa30317@seeu.edu.mk>
2024-03-02 11:17:43 +0100

    Added the detailed contact page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(ContactsApp());
 }
@@ -17,6 +18,147 @@ class ContactsApp extends StatelessWidget {
   }
 }
 
+class ContactDetailScreen extends StatelessWidget {
+  final Contact contact;
+
+  ContactDetailScreen(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact Details'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blue, // You can customize the color
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        contact.name[0], // Initial letter of the contact's name
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Center(
+                    child: Text(
+                      contact.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Center(
+                    child: Text(
+                      'Phone: ${contact.phoneNumber}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.call, color: Colors.green),
+                        onPressed: () {
+                          // Perform call action
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.message, color: Colors.blue),
+                        onPressed: () {
+                          // Perform message action
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.video_call, color: Colors.red),
+                        onPressed: () {
+                          // Perform video call action
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Viber',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.videocam, color: Colors.purple),
+                    onPressed: () {
+                      // Perform Viber video call action
+                    },
+                  ),
+                  Text(
+                    'WhatsApp',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.video_call, color: Colors.green),
+                    onPressed: () {
+                      // Perform WhatsApp video call action
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle History button press
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[300]!),
+                ),
+                child: Text('History'),
+              ),
+            ),
+            SizedBox(height: 8),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle Storage locations button press
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[300], // Grey background color
+                ),
+                child: Text('Storage locations'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
 class ContactsPage extends StatefulWidget {
   @override
   _ContactsPageState createState() => _ContactsPageState();
@@ -24,6 +166,7 @@ class ContactsPage extends StatefulWidget {
 
 class _ContactsPageState extends State<ContactsPage> {
   String _searchQuery = '';
+  Contact? _selectedContact;
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +175,14 @@ class _ContactsPageState extends State<ContactsPage> {
       return contact.name.toLowerCase().contains(_searchQuery.toLowerCase()) ||
           contact.phoneNumber.contains(_searchQuery);
     }).toList();
+
+    // Group contacts by the first letter of their names
+    Map<String, List<Contact>> groupedContacts = {};
+    for (var contact in filteredContacts) {
+      String firstLetter = contact.name[0].toUpperCase();
+      groupedContacts.putIfAbsent(firstLetter, () => []);
+      groupedContacts[firstLetter]!.add(contact);
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -48,102 +199,102 @@ class _ContactsPageState extends State<ContactsPage> {
         ),
       ),
       body: ListView.builder(
-        itemCount: filteredContacts.length,
+        itemCount: groupedContacts.length,
         itemBuilder: (context, index) {
-          final contact = filteredContacts[index];
-          // Check if it's the first contact with this initial
-          final bool isFirstContactWithInitial =
-              index == 0 || contact.name[0] != filteredContacts[index - 1].name[0];
-
+          String initialLetter = groupedContacts.keys.elementAt(index);
+          List<Contact> contacts = groupedContacts[initialLetter]!;
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if (isFirstContactWithInitial)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: Text(
-                    contact.name[0],
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              GestureDetector(
-                onTap: () {
-                  // Navigate to detail page
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ContactDetailScreen(contact),
-                    ),
-                  );
-                },
-                child: ListTile(
-                  title: Text(contact.name),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.call),
-                        onPressed: () {
-                          // Perform call action
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.message),
-                        onPressed: () {
-                          // Perform message action
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.video_call),
-                        onPressed: () {
-                          // Perform video call action
-                        },
-                      ),
-                      Icon(Icons.info),
-                    ],
-                  ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(
+                  initialLetter,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
-              Divider(),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  final contact = contacts[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedContact = contact;
+                          });
+                        },
+                        child: ListTile(
+                          title: Text(contact.name),
+                        ),
+                      ),
+                      if (_selectedContact == contact) ...[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Phone: ${contact.phoneNumber}',
+                                textAlign: TextAlign.left,
+                              ),
+                              SizedBox(height: 8),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    iconSize: 40, // Increase icon size
+                                    icon: Icon(Icons.call, color: Colors.green),
+                                    onPressed: () {
+                                      // Perform call action
+                                    },
+                                  ),
+                                  IconButton(
+                                    iconSize: 40, // Increase icon size
+                                    icon: Icon(Icons.message, color: Colors.blue),
+                                    onPressed: () {
+                                      // Perform message action
+                                    },
+                                  ),
+                                  IconButton(
+                                    iconSize: 40, // Increase icon size
+                                    icon: Icon(Icons.video_call, color: Colors.red),
+                                    onPressed: () {
+                                      // Perform video call action
+                                    },
+                                  ),
+                                  IconButton(
+                                    iconSize: 40, // Increase icon size
+                                    icon: Icon(Icons.info, color: Colors.orange), // Information icon
+                                    onPressed: () {
+                                      // Navigate to detail page
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              ContactDetailScreen(_selectedContact!),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                      Divider(), // Add a divider between contacts
+                    ],
+                  );
+                },
+              ),
             ],
           );
         },
-      ),
-    );
-  }
-}
-
-class ContactDetailScreen extends StatelessWidget {
-  final Contact contact;
-
-  ContactDetailScreen(this.contact);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contact Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              contact.name,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Phone: ${contact.phoneNumber}',
-              style: TextStyle(fontSize: 16),
-            ),
-            // Add more fields as needed (e.g., email, address, etc.)
-          ],
-        ),
       ),
     );
   }
